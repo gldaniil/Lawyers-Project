808470f964d1aee33299c9d38671e7cb09b41b1c
gldaniil <daniil.gajfullin@yandex.ru>
2023-03-14 15:45:54 +0500

    Раскрывающееся подменю

--- js/header.js
+++ js/header.js
@@ -2,18 +2,24 @@ const hamburger = document.querySelector("div.hamburger");
 const headerMenu = document.querySelector("nav.bottom-menu");
 
 hamburger.addEventListener('click', () => {
-  headerMenu.classList.contains('active')
-    ? headerMenu.classList.remove('active')
-    : headerMenu.classList.add('active')
+  headerMenu.classList.toggle('active')
+  
+  if (subMenuAboutUs.classList.contains('active')) subMenuAboutUs.classList.remove('active') 
+  if (subMenuServices.classList.contains('active')) subMenuServices.classList.remove('active')
 });
 
-const aboutUs = document.querySelector("li#about-us");
-const subMenuAboutUs = document.querySelector("li#about-us ul");
+const aboutUs = document.querySelector("#li-about-us");
+const subMenuAboutUs = document.querySelector("#li-about-us ul");
 
 aboutUs.addEventListener('click', () => {
-  subMenuAboutUs.classList.contains('active')
-    ? subMenuAboutUs.classList.remove('active')
-    : subMenuAboutUs.classList.add('active')
+  subMenuAboutUs.classList.toggle('active')
 });
+
+const services = document.querySelector("#li-services");
+const subMenuServices = document.querySelector("#li-services ul");
+
+services.addEventListener('click', () => {
+  subMenuServices.classList.toggle('active')
+})
 
 const clickedLink = () => ( false );
